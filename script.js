3194e80ea96b3123c7c6abcf491a64d1ca44720a
Bhoma Ram <120722224+Bhoma-RamDaiya@users.noreply.github.com>
2024-01-19 16:14:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ function parseCode(encodedString) {
 }
 
 // Examples
-console.log(parseCode("John000Doe000123"));
+// console.log(parseCode("John000Doe000123"));
 // Output: { firstName: "John", lastName: "Doe", id: "123" }
 
-console.log(parseCode("michael0smith004331"));
+// console.log(parseCode("michael0smith004331"));
 // Output: { firstName: "michael", lastName: "smith", id: "4331" }
 
-console.log(parseCode("Thomas00LEE0000043"));
+// console.log(parseCode("Thomas00LEE0000043"));
 // Output: { firstName: "Thomas", lastName: "LEE", id: "43" }
 
 // Do not change the code below
